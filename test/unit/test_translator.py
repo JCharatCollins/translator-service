c7168c28236b351d6df1b87d7dbeb66c057d5957
Joshua Charat-Collins <jcharatcollins@gmail.com>
2024-04-01 17:01:43 -0400

    Take 10.

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -202,12 +202,15 @@ def eval_single_response_complete(expected_answer: "tuple[bool, str]", llm_respo
 
 #   return sum(eval_results)/len(eval_results)
 
-@patch('src.translator.translate_content')
 def test_individual(test, mocker):
   mocker.return_value = test["expected_answer"]
-  llm_response = src.translator.translate_content(test["post"])
-  assert eval_single_response_complete(test["expected_answer"], llm_response) >= 0.8
 
 def test_all():
   for test in complete_eval_set:
-    test_individual(test)
+    @patch('src.translator.translate_content')
+    def test_individual(mocker):
+        mocker.return_value = test["expected_answer"]
+        llm_response = src.translator.translate_content(test["post"])
+        assert eval_single_response_complete(test["expected_answer"], llm_response) >= 0.8
+    test_individual()
+
